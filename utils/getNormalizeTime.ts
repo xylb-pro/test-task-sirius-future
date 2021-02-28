3707660022cdd75104b953b7d4fe9aea78a8ede9
xylb-pro <churakin.kirill@gmail.com>
2021-02-28 08:06:24 +0300

    fix: slight performance improvement

--- utils/getNormalizeTime.ts
+++ utils/getNormalizeTime.ts
@@ -1,6 +1,7 @@
 export const getNormalizeTime = (time: number) => {
-  const seconds = Math.floor((time / 1000) % 60);
-  const minutes = Math.floor((time / 1000 / 60) % 24);
+  const toSec = Math.floor(time / 1000);
+  const seconds = toSec % 60;
+  const minutes = Math.floor((toSec / 60) % 60);
   if (minutes > 0) return `${minutes} min ${seconds} sec`;
   else return `${seconds} sec`;
 };
