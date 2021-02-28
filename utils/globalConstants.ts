b2b4a7888d3d3f8cc656c75bfd88c7df4015c99d
xylb-pro <churakin.kirill@gmail.com>
2021-02-28 08:05:25 +0300

    changes: add comments

--- utils/globalConstants.ts
+++ utils/globalConstants.ts
@@ -1,11 +1,24 @@
 export const MAX_STAR_COUNT: number = 3;
 export const MAX_X_POSITION: number = 690;
 export const MAX_Y_POSITION: number = 850;
+/**
+ * Star step in pixels
+ */
 export const STAR_STEP: number = 2;
-export const TIME_OUT = {
+/**
+ * Вelay between spawning stars at start
+ */
+export const BASE_SPAWN_DELAY: number = 3000;
+/**
+ * Spawn coordinates spread of the star
+ */
+export const SPAWN_AREA = {
   min: -100,
-  max: -300,
+  max: -400,
 };
+/**
+ * Possible star values
+ */
 export const VALUE = {
   min: -5,
   max: 5,
