68c0bff11f38fe17530cd1bd38e833ad815079fa
xylb-pro <churakin.kirill@gmail.com>
2021-02-28 08:07:52 +0300

    fix: now correct star value

--- utils/createStar.ts
+++ utils/createStar.ts
@@ -1,12 +1,16 @@
-import { MAX_X_POSITION, TIME_OUT, VALUE } from './globalConstants';
+import { MAX_X_POSITION, SPAWN_AREA, VALUE } from './globalConstants';
 
 export const createStar = (addToStorage) => {
   const X = Math.random() * MAX_X_POSITION;
   const timeOut = Math.floor(
-    Math.random() * (TIME_OUT.max - TIME_OUT.min + 1) + TIME_OUT.min,
+    Math.random() * (SPAWN_AREA.max - SPAWN_AREA.min + 1) + SPAWN_AREA.min,
   );
-  const starValue =
-    Math.floor(Math.random() * (VALUE.max - VALUE.min + 1) + VALUE.min) | 1;
+  let starValue = VALUE.without;
+  while (starValue === VALUE.without) {
+    starValue = Math.floor(
+      Math.random() * (VALUE.max - VALUE.min + 1) + VALUE.min,
+    );
+  }
   addToStorage((prev) => {
     prev.push({ x: X, y: timeOut, value: starValue });
     return prev;
